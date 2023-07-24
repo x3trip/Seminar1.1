824e0038b7a70c8f95ab6664c32b2b250904ffb8
Ilya <iljafff@mail.ru>
2023-07-25 02:34:32 +0300

    Домашняя работа по первому семинару

---
+++ Program.cs
@@ -0,0 +1,95 @@
+﻿
+
+/*
+Написать программу где в нее входят два числа и выйдет мак и мин среди них
+
+Console.Write("input first number: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input second namber: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+
+if (num1 > num2)
+{
+    Console.WriteLine($"max {num1} min {num2}");
+}
+else
+{
+    Console.WriteLine($"max {num2} min {num1}");
+}
+*/
+
+
+
+/*
+Написать программу которая принимает 3 числа и выдает макс из них
+
+Console.Write("input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input num2: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("input num3: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
+
+int MAX = num1;
+
+if(num2 > MAX)
+{
+    Console.WriteLine(num2);
+}
+else
+{
+    if(num3 > MAX)
+    {
+       Console.WriteLine(num3); 
+    }
+    else
+    {
+       Console.WriteLine(num1);
+    }
+}
+*/
+
+
+/*
+Проверка числа на четность
+
+Console.Write("input num1: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+if(num1 % 2==0)
+{
+    Console.WriteLine("YES");
+}
+else
+{
+    Console.WriteLine("NO");
+}
+*/
+
+
+
+/*
+Console.Write("input number: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int current = 1;
+
+while(current <= num)
+{
+    if(current % 2==0)
+    {
+        Console.Write(current + " ");
+        current++;
+    }
+    else
+    {
+        Console.Write(" ");
+        current++;
+    }
+    
+    
+}
+*/
+
+
+
